ead463e62ee59116c2a02d89b68d5a1660045762
Dzx1025 <dzxdzx12345@gmail.com>
2021-09-02 15:53:59 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Computer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Computer.dir/main.cpp.obj"
+  "Computer.exe"
+  "Computer.exe.manifest"
+  "Computer.pdb"
+  "libComputer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Computer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
